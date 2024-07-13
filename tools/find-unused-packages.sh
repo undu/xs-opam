f18e853348cdf79379773a69035764aa0c7e52bf
Pau Ruiz Safont <unduthegun@gmail.com>
2024-07-13 19:33:32 +0100

    tools: adapt find-unused packages to the new layout
    And remove unused packages.
    
    Some ocaml packages had to be added, they were previously ignored by the
    command.
    
    For some unknown reason dlm is considered unused, but xs-toolstack
    indirectly depends on it.

--- tools/find-unused-packages.sh
+++ tools/find-unused-packages.sh
@@ -7,9 +7,7 @@ export LC_ALL=C
 
 export OPAMCOLOR=NEVER
 
-USED=$(opam list -t --required-by xs-toolstack --recursive --short | sort)
-UPSTREAM=$(find "packages/upstream/" -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 -I {} echo "{}" | cut -d "/" -f 3 | cut -d "." -f 1 | sort | uniq)
-XS=$(find "packages/xs/" -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 -I {} echo "{}" | cut -d "/" -f 3 | cut -d "." -f 1 | sort | uniq)
-diff -u .github/unused-upstream.txt <(comm -23 <(echo "$UPSTREAM") <(echo "$USED"))
-diff -u .github/unused-xs.txt <(comm -23 <(echo "$XS") <(echo "$USED"))
+USED=$(opam list -t --required-by xs-toolstack --or --required-by dev-tools --recursive --short | sort)
+PACKAGES=$(find "packages/" -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 -I {} echo "{}" | cut -d "/" -f 2 | sort | uniq)
+diff -u .github/unused-packages.txt <(comm -23 <(echo "$PACKAGES") <(echo "$USED"))
 echo OK
